b3dea2bd406d41d5668ded498661b4507217261f
RANGSIMAN KETKAEW <rangsiman1993@gmail.com>
2017-03-21 13:49:18 +0700

    Create repeat_number.f90
    Print the inter number from 1 to n.

---
+++ repeat_number.f90
@@ -0,0 +1,20 @@
+module module1
+integer:: n
+contains
+recursive subroutine sub1(x)
+integer,intent(inout):: x
+if (x < n) then
+x = x + 1
+print *, 'x = ', x
+call sub1(x)
+end if
+end subroutine sub1
+end module module1
+
+PROGRAM MAIN
+use module1
+integer:: x = 0
+print *, 'Enter number of repeats'
+read (*,*) n
+call sub1(x)
+END PROGRAM MAIN
